3b0049944394f758d018465dcfd97447053248cf
MrJiang <zsgwsjj@qq.com>
2018-12-30 14:10:23 +0800

    version 1.0.5

--- src/comm.js
+++ src/comm.js
@@ -1,3 +1,3 @@
-// export const apiDomain = 'http://39.104.24.216:8111';
-export const apiDomain = 'http://localhost:8111';
+export const apiDomain = 'http://39.104.24.216:8111';
+// export const apiDomain = 'http://localhost:8111';
 
